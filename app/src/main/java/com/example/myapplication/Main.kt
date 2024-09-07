ee65fd7c43ae41a1957f3e013d3129ef3c91d52e
Anastasia <peretat21@gmail.com>
2024-09-07 12:30:28 +0300

    Commit

---
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -0,0 +1,65 @@
+package com.example.myapplication
+
+import java.time.LocalDate
+import java.time.Month
+
+
+//1 задача вариант 3
+
+/*/import java.util.Calendar
+
+fun main() {
+    val c = Calendar.getInstance()
+
+    val month = c.get(Calendar.MONTH)
+    val day = c.get(Calendar.DAY_OF_WEEK)
+
+    val dayt = ("Суббота")
+    val mounss=( "Сентябрь")
+    val names =("Анастасия")
+    println("$day - $dayt")
+    println("$month - $mounss")
+    println(names)
+}
+ */
+
+//2 задача вариант 18
+/*/
+fun main() {
+    var date = LocalDate.parse("2018-12-31")
+    val date2 = LocalDate.parse("2020-12-30")
+
+    if (date > date2) {
+        println("No")
+    }
+    else{
+        println("Yes")
+    }
+
+    var year = date.year
+    var monts = date.month
+    var day = date.dayOfMonth
+
+}
+*/
+fun main(){
+    var numberq = 154
+
+        var des = numberq % 10
+        var sot = numberq % 100 / 10
+        var tis = numberq / 100
+        print("$des,$sot,$tis")
+    var n=0
+    var ten=10
+    for(numberq in 1 .. 3){
+ var perem=numberq/ten%100
+
+           n++
+
+        }
+    println(n)
+        }
+
+
+
+
