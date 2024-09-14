a74f55a25c654f035929624906abfb139e7d3af0
Anastasia <peretat21@gmail.com>
2024-09-14 10:46:45 +0300

    Lab2

---
+++ app/src/main/java/com/example/myapplication/lab2.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+class lab2 {
+    fun task1(nember:Int):Int {
+        if(nember%2==0){
+        return nember}
+        else{
+            return nember*2
+        }
+    }
+        fun main(){
+           var number = 54
+            task1((number))
+            print(number)
+        }
+    }
