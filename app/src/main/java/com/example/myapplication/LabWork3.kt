c0e39d12335f1bfdb0527e466fde1888e8d004ee
Anastasia <peretat21@gmail.com>
2024-09-28 09:28:21 +0300

    1232

--- app/src/main/java/com/example/myapplication/LabWork3.kt
+++ app/src/main/java/com/example/myapplication/LabWork3.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+//задание 1
 fun task1() {
     var nullableVariable: Int? = null
     println("Nullable variable: $nullableVariable")
@@ -7,3 +8,19 @@ fun task1() {
     val text: String = nullableVariable?.toString() ?: "Variable is null"
     println("Text: $text")
 }
+//задание 2
+
+fun GetFullName(firstName:String?, lastName:String?):String{
+    val firstNameString= firstName?.trim()?:"dddd"
+    val lastNameString=lastName?.trim()?:"sss"
+    return "$firstNameString,$lastNameString"
+}
+fun task2(){
+    println("Введите имя:")
+    val name=readLine()
+    println("Введите фамилию:")
+    val lastName= readLine()
+    val allName= GetFullName(name,lastName)
+}
+//задание 3
+
