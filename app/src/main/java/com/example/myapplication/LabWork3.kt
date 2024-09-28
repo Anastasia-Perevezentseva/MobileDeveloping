709c696343b75d0c9a2f679b99addfd6f3a339f9
Anastasia <peretat21@gmail.com>
2024-09-28 10:40:41 +0300

    1232

--- app/src/main/java/com/example/myapplication/LabWork3.kt
+++ app/src/main/java/com/example/myapplication/LabWork3.kt
@@ -1,26 +1,54 @@
 package com.example.myapplication
 
 //задание 1
-fun task1() {
-    var nullableVariable: Int? = null
-    println("Nullable variable: $nullableVariable")
-    nullableVariable = 10
-    val text: String = nullableVariable?.toString() ?: "Variable is null"
-    println("Text: $text")
-}
+//fun main() {
+//    var nullableVariable: Int? = null
+//    println("нулевая: $nullableVariable")
+//    nullableVariable = 10
+//    val text: String = nullableVariable?.toString() ?: "Unknown"
+//    println("$text")
+//}
 //задание 2
-
-fun GetFullName(firstName:String?, lastName:String?):String{
-    val firstNameString= firstName?.trim()?:"dddd"
-    val lastNameString=lastName?.trim()?:"sss"
-    return "$firstNameString,$lastNameString"
+//
+//fun GetFullName(firstName:String?, lastName:String?):String{
+//    val firstNameString= firstName?.trim()?:"Unknown"
+//    val lastNameString=lastName?.trim()?:"Unknown"
+//    return "$firstNameString $lastNameString"
+//}
+//fun main(){
+//    print("Введите имя:")
+//    val name=readLine()
+//    print("Введите фамилию:")
+//    val lastName= readLine()
+//    val allName= GetFullName(name,lastName)
+//    println(allName)
+//}
+////задание 3
+//
+//
+//fun calculateSquareRoot(number: Double?): Double{
+//    return number!!.toDouble().let {
+//        if (it < 0) {
+//            throw RuntimeException("")
+//        } else {
+//            Math.sqrt(it)
+//        }
+//    }
+//}
+//fun main(){
+//    println("Введите число:")
+//    val number:Double = readLine()!!.toDouble()
+//    val numberComplit= calculateSquareRoot(number)
+//    print(numberComplit)
+//}
+//// задание 4
+//
+fun getStringLength(stringg:Any?):Int {
+    return (stringg as? String)?.length ?: -1
 }
-fun task2(){
-    println("Введите имя:")
-    val name=readLine()
-    println("Введите фамилию:")
-    val lastName= readLine()
-    val allName= GetFullName(name,lastName)
+fun main(){
+    println("Введите строку:")
+    val Stringg= readLine()
+    val ASS= getStringLength(Stringg)
+    println(ASS)
 }
-//задание 3
-
