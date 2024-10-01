9de80620df1fc280daf17f50f55506116d436d76
Anastasia <peretat21@gmail.com>
2024-10-01 10:16:43 +0300

    1232

--- app/src/main/java/LabWork4.kt
+++ app/src/main/java/LabWork4.kt
@@ -1,14 +1,28 @@
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+package com.example.helloapp
 
-@Composable
-fun SecondScreen() {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(horizontalAlignment = Alignment.End, modifier = Modifier.fillMaxSize())
+            {
+                Text("Kotlin", fontSize = 28.sp)
+                Text("Java", fontSize = 28.sp)
+                Text("JavaScript", fontSize = 28.sp)
+                Text("Python", fontSize = 28.sp)
+            }
+        }
+    }
 }
 
-@Preview
-@Composable
-private fun preview() {
-    SecondScreen()
-}
-fun main(){}
